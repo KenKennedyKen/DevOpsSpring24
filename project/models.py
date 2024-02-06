8a7eadf2695d18ef99bed8003e3b22d16beb8bd5
Brock <brock.nicoletti@umontana.edu>
2024-02-06 18:20:21 +0000

    And more 'register' stuff.

--- project/models.py
+++ project/models.py
@@ -1,4 +1,5 @@
 from project.app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Post(db.Model):
@@ -12,3 +13,18 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<title {self.title}>"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
